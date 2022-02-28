2a820eaa6bb30cd7f3e37b0cac9f3bfde7f7c3bb
mrjordanov <marin.jordanov@gmail.com>
2022-02-28 16:25:24 +0200

    Add change

--- 08.Objects and Classes-Exercise/src/OrderByAge/PeopleMain.java
+++ 08.Objects and Classes-Exercise/src/OrderByAge/PeopleMain.java
@@ -7,24 +7,23 @@ import java.util.Scanner;
 public class PeopleMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input= scanner.nextLine();
+        String input = scanner.nextLine();
 
-        List<People> information=new ArrayList<>();
-        while(!input.equals("End")){
-            String[] info=input.split(" ");
-            String name=info[0];
-            String id=info[1];
-            int age=Integer.parseInt(info[2]);
+        List<People> information = new ArrayList<>();
+        while (!input.equals("End")) {
+            String[] info = input.split(" ");
+            String name = info[0];
+            String id = info[1];
+            int age = Integer.parseInt(info[2]);
 
-            People people= new People(name,id,age);
+            People people = new People(name, id, age);
             information.add(people);
-            input= scanner.nextLine();
+            input = scanner.nextLine();
         }
 
 
-        information.stream().sorted((s1,s2)->Integer.compare(s1.getAge(),s2.getAge())).
-                forEach(s-> System.out.println(s));
-
+        information.stream().sorted((s1, s2) -> Integer.compare(s1.getAge(), s2.getAge())).
+                forEach(s -> System.out.println(s));
 
 
     }
